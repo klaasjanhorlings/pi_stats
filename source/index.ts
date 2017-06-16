eb8253b9bed5e74d5d2b3aa323b6d30bc1bafa33
Klaasjan Horlings <k.horlings@gmail.com>
2017-06-16 16:44:20 +0200

    Added delta and relative logic

---
+++ source/index.ts
@@ -0,0 +1,31 @@
+import { readCpuValues, getDelta, getRelative } from "./proc-stat";
+
+const timeout = (length: number) => (
+    new Promise(resolve => {
+        setTimeout(() => resolve(), length);
+    })
+);
+
+const log = () => (
+    new Promise(resolve => {
+        console.log("Resolve");
+        resolve();
+    })
+);
+
+var first, second;
+readCpuValues().then(values => {
+    console.log("recorded first value");
+    first = values;
+    return timeout(5000);
+}).then(() => readCpuValues())
+.then(values => {
+    console.log("recorded second value");
+    second = values;
+    const delta = getDelta(first, second);
+    const relative = getRelative(delta);
+    console.log("delta:");
+    console.log(delta);
+    console.log("relative:");
+    console.log(relative);
+})
--- source/proc-stat.ts
+++ source/proc-stat.ts
@@ -1,22 +1,47 @@
 import winston from "winston";
-import * as util from "util";
+import { promisify } from "util";
+import { readFile } from "fs";
 import { execFile } from "child_process";
 
-execFile("cat", ["/proc/stat"], (err, stdout, stderr) => {
-    if (err) {
-        winston.error(`Something went wrong while retrieving /proc/stat information. (Message: ${err})`);
-        return;
-    }
+const readFileAsync = promisify(readFile);
 
-    console.log(processStatOutput(stdout));
-});
-
-const processStatOutput = (output: string) => {
-    const lines = output.split(/\r|\n|\r\n]/);
-    return lines.filter(l => l.startsWith("cpu")).map(l => processCpuLine(l));
+export async function readCpuValues() {
+    const rawValues = await readFileAsync("/proc/stat", "utf8");
+    return processStatOutput(rawValues);
 }
 
-const processCpuLine = (line: string) => {
+export const getDelta = (prev: CpuStats, current: CpuStats): CpuStats => ({
+    name:   current.name,
+    user:   current.user - prev.user,
+    nice:   current.nice - prev.nice,
+    system: current.system - prev.system,
+    idle:   current.idle - prev.idle,
+    iowait: current.iowait - prev.iowait,
+    irq:    current.irq - prev.irq,
+    softirq: current.softirq - prev.softirq
+});
+
+export const getRelative = (stats: CpuStats): CpuStats => {
+    const total = stats.user + stats.nice + stats.system + stats.idle
+                + stats.iowait + stats.irq + stats.softirq;
+    return {
+        name: stats.name,
+        user: stats.user / total,
+        nice: stats.nice / total,
+        system: stats.system / total,
+        idle: stats.idle / total,
+        iowait: stats.iowait / total,
+        irq: stats.irq / total,
+        softirq: stats.softirq / total,
+    };
+}
+
+const processStatOutput = (output: string): CpuStats[] => {
+    const lines = output.split(/\r|\n|\r\n]/);
+    return lines.filter(l => l.startsWith("cpu")).map(l => processCpuLine(l));
+};
+
+const processCpuLine = (line: string): CpuStats => {
     const words = line.split(/\s+/);
     if (!words[0].startsWith("cpu")) {
         return;
@@ -32,4 +57,15 @@ const processCpuLine = (line: string) => {
         irq:    parseInt(words[6]),
         softirq: parseInt(words[7])
     };
+};
+
+interface CpuStats {
+    name:   string;
+    user:   number;
+    nice:   number;
+    system: number;
+    idle:   number;
+    iowait: number;
+    irq:    number;
+    softirq: number;
 }
