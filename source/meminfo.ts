ec421d4fedc9cd3556cc262ef68bde0e43296871
Klaasjan Horlings <k.horlings@gmail.com>
2017-06-19 03:08:11 +0200

    Corrected copy pasta error

--- source/meminfo.ts
+++ source/meminfo.ts
@@ -1,7 +1,7 @@
 import log from "./logging";
 import { readFile } from "fs";
 
-const memInfoPath = "/proc/stat";
+const memInfoPath = "/proc/meminfo";
 
 export const readMemInfo = () => (
     new Promise<MemoryStats>((resolve, reject) => {
@@ -20,6 +20,7 @@ const processMeminfoOutput = (output: string) => {
     const lines = output.split(/\r|\n|\r\n]/);
     const result: Partial<MemoryStats> = {};
     for(let line of lines) {
+        console.log(line);
         if (line.startsWith("MemTotal")) {
             result.total = parseInt(line.match(/(\d+)/)[1]);
         } else if (line.startsWith("MemFree")) {
